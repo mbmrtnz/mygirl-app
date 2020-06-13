a9660daa7eeff253a09818f727910cac42e2eff7
bryan <m.bryanmartinez1@gmail.com>
2020-06-13 21:30:11 +0800

    wrapper and sample components

---
+++ src/app/_components/common/modal/modal.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit, Input, EventEmitter, Output, AfterViewInit, ViewChild, OnDestroy} from '@angular/core';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit, OnDestroy {
+
+  @Input() mdlConfig = {
+    mdlType: "",
+    mdlOpener: "",
+    mdlBtnAlign: "",
+    mdlSize: "",
+    confirmationMsg: "",
+    successMsg: ""
+  };
+  /*
+    mdlType: confirmation, success, error, custom       DEFAULT: custom
+    mdlOpener: button, a, div, icon;                    DEFAULT: button
+    mdlBtnAlign: left, center, right;                   DEFAULT: right
+    mdlSize: sm, md, lg;                                DEFAULT: md
+    mdlTitle: ""                                        DEFAULT: null
+
+  */
+  @ViewChild('content', null) test: any;
+
+  @Input() modalSize: string = "modal-size";
+  @Input() btnTitle: string = "Open Modal from Child";
+  @Input() disabled: boolean = false;
+  @Output() modalOpened: EventEmitter<Object> = new EventEmitter<Object>();
+  
+  content: EventEmitter<Object> = new EventEmitter<Object>();
+  modalRef: NgbModalRef;
+  mdlOptions: any;
+
+  constructor(private ms: NgbModal) { }
+
+  ngOnInit() {
+  	this.mdlOptions = {
+      centered: true, 
+      backdrop: 'static', 
+      windowClass : this.modalSize
+  	}
+  }
+
+  ngOnDestroy() {
+  	if(this.modalRef != undefined){
+      this.modalRef.close();
+    }
+  }
+
+  openMdl(content?) {
+  	this.modalRef = this.ms.open(this.test, this.mdlOptions);
+  	this.modalOpened.emit();
+  }
+
+  closeMdl() {
+  	this.modalRef.dismiss();
+    this.modalRef = undefined;
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './_components/home/home.component';
+import { AboutComponent } from './_components/about/about.component';
+import { ModalComponent } from './_components/common/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
@@ -18,8 +25,11 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     NgbModule,
     AppRoutingModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
