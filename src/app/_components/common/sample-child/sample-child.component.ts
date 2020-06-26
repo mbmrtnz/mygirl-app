54ef0c1a8b010061543f992af5d757538e31db7c
bryan <m.bryanmartinez1@gmail.com>
2020-06-27 04:10:06 +0800

    sample

--- src/app/_components/common/sample-child/sample-child.component.ts
+++ src/app/_components/common/sample-child/sample-child.component.ts
@@ -7,13 +7,77 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class SampleChildComponent implements OnInit {
 
-  @Input() sampleInput = "";
+  @Input() sampleInput: any = null;
   @Output() sampleOutput = new EventEmitter();
+
+  obj: any = {
+  	productArr: [
+	  				{
+				  		name: "Milktea 1",
+				  		sizeCode: "M",
+				  		sizeName: "Medium",
+				  		price: 125.0,
+				  		obj2: {
+				  			topping: 'oreo'
+				  		}
+				  	},
+				  	{
+				  		name: "Milktea 2",
+				  		sizeCode: "S",
+				  		sizeName: "Small",
+				  		price: 100.0,
+				  		obj2: {
+				  			topping: 'cookie'
+				  		}
+				  		// obj2: [4,3,2,1]
+				  	},
+				  	{
+				  		name: "Milktea 3",
+				  		sizeCode: "M",
+				  		sizeName: "Medium",
+				  		price: 150.0,
+				  		obj2: {
+				  			topping: 'puki'
+				  		}
+				  		// obj2: [9,9,9,9]
+				  	}
+			  	]
+  }
+
+  productArr: any[] = [
+	  	{
+	  		name: "Milktea 1",
+	  		sizeCode: "M",
+	  		sizeName: "Medium",
+	  		price: 125.0
+	  	},
+	  	{
+	  		name: "Milktea 2",
+	  		sizeCode: "S",
+	  		sizeName: "Small",
+	  		price: 100.0
+	  	},
+	  	{
+	  		name: "Milktea 3",
+	  		sizeCode: "M",
+	  		sizeName: "Medium",
+	  		price: 150.0
+	  	}
+  	];
 
   constructor() { }
 
   ngOnInit() {
-  	this.sampleOutput.emit('from child to parent');
+  	console.log(this.productArr)
+  }
+
+  onClick() {
+  	this.sampleOutput.emit({
+  		prop1: "Property 1",
+  		prop2: "Property 2",
+  		prop3: "Property 3",
+  		prop4: "Property 4"
+  	});
   }
 
 }
--- src/app/_components/common/sample-parent/sample-parent.component.ts
+++ src/app/_components/common/sample-parent/sample-parent.component.ts
@@ -7,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SampleParentComponent implements OnInit {
 
-  toChild: string = 'from parent to child';
-  fromChild: string = "";
+  toChild = {
+  	attr1: "Test 1",
+  	attr2: "Test 2"
+  };
+
+  fromChild: any = {};
+  var1: any = null;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  myFunction(ev) {
+  	console.log(ev);
+  	this.fromChild = ev;
   }
 
 }
